56ef403cf8158226c519e1d14a26f47018cd1ada
palsapna681-gif <palsapna681@gmail.com>
2026-09-18 14:54:16 +0530

    Create sw.js

---
+++ static/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "container-design-v1";
+
+const APP_SHELL = ["/"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
